9ebc0be2ed6c4e696861327a97ccf6462f853403
LeeMir <manofgodmj@gmail.com>
2022-07-17 14:30:50 +0900

    [Chore] Init Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Time Bar
+
+Parcel + React + Typescript로 만드는 클릭형 Time Bar
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.scss';
+
+const App = () => {
+  return (
+    <div id="App">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
